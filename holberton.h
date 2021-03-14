916298fa887e9c88868d0187c099bc7b518cf77e
Carson Stearn <cstearn@gmail.com>
2021-03-14 00:18:03 +0000

    adding .h file, v1

---
+++ holberton.h
@@ -0,0 +1,25 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h> // variadic functions
+#include <unistd.h> // system, read, write
+
+typedef struct printf_variables
+{
+
+
+
+
+}
+
+
+
+int _putchar(char c);
+
+int printf_c();
+int printf_str();
+int printf_int();
+
+#endif
