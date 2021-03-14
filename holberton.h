1e65dbfd9f172905b0d819a603060401aedd2e27
Carson Stearn <cstearn@gmail.com>
2021-03-14 16:46:46 +0000

    updated .h

--- holberton.h
+++ holberton.h
@@ -6,20 +6,34 @@
 #include <stdarg.h> // variadic functions
 #include <unistd.h> // system, read, write
 
-typedef struct printf_variables
+
+/**
+ * struct format - struct for printf format
+ */
+typedef struct format
 {
-
-
+	char *f;
 
 
 }
 
 
-
+/* helpers */
 int _putchar(char c);
 
+
+/* main functions */
 int printf_c();
 int printf_str();
 int printf_int();
+int printf_bin();
+int prtinf_unsign_int();
+int printf_oct();
+int printf_hex();
+int printf_HEX();
+int printf_ptr();
+int printf_special_string();
+int printf_str_rev();
+int printf_rot();
 
 #endif
