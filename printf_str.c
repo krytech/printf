e39b0d96bcfa33a84c47a54269b1f693f2b9afbd
Lucas Lazarre <lucaslazarre@Lucass-MacBook-Air.local>
2021-03-16 14:31:21 -0400

    uploading the printf string

---
+++ printf_str.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * printf_str - Print the string
+ * @s: string
+ * Return: String
+ */
+
+int printf_str(va_list)
+{
+	char s;
+	s = va_arg(val, str);
+
+	while(*s)
+	_putchar(*s);
+	return (0);
+}
