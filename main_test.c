f10a107e0f363cbb12c71025f1f5f69c762b6002
Lucas Lazarre <lucaslazarre@Lucass-MacBook-Air.local>
2021-03-16 21:09:47 -0400

    Trial and error for the int

--- main_test.c
+++ main_test.c
@@ -9,7 +9,8 @@ int main(void)
 	printf("Percent:[%%]\n");
 	_printf("String:[%s]\n", "I am a string !");
 	printf("String:[%s]\n", "I am a string !");
-
+	_printf("Length:[%d, %i]\n", 5, 10);
+	printf("Length:[%d, %i]\n", 5, 10);
 
 	return (0);
 }
