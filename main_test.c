19d0148b2f269aa8a3a5cec57689025f50ec92ab
Carson Stearn <cstearn@gmail.com>
2021-03-17 00:05:58 +0000

    adding good test file

---
+++ main_test.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+int main(void)
+{
+
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+	_printf("Percent:[%%]\n");
+	printf("Percent:[%%]\n");
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+
+
+	return (0);
+}
